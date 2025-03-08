640b5b646d007d27331d7d3517a04bef77d25ea9
Amalia Kadoi <amaliakadoi@gmail.com>
2025-03-08 11:37:44 +0800

    Delete Practical 1 directory
    Moved to another repository 'practice_python_program

--- Practical 1/BiggerNumber.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-if num1 > num2:
-    print(f"The bigger number is {num1}.")
-elif num1 < num2:
-    print(f"The bigger number is {num2}")
-else:
-    print("Both numbers re equal.")
--- Practical 1/Equal.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-if num1 == num2:
-    print("Equal")
-else:
-    print("Not Equal")
